c2b31084fd5634265f60026afecbd2ecf7affe6d
facundo@taniquetil.com.ar <>
2009-03-22 19:18:40 -0300

    Infrastructura andando.

--- core/tests/test_dispatcher.py
+++ core/tests/test_dispatcher.py
@@ -1,25 +1,37 @@
 
-import unittest
+from twisted.trial.unittest import TestCase as TwistedTestCase
+from twisted.internet import defer
 
 from core import events
 from core import dispatcher
 
-class TestRegister(unittest.TestCase):
+class TestRegister(TwistedTestCase):
 
-   * register ('event_<>', callback)
-              ('event_command', callback, [c])
-              ('privmsg', callback, [regex])
+#   * register ('event_<>', callback)
+#              ('event_command', callback, [c])
+#              ('privmsg', callback, [regex])
+#
+    def setUp(self):
+        self.d = defer.Deferred()
+        self.disp = dispatcher.Dispatcher()
+        self.timeout = 1
+
+    def deferredAssertEqual(self, deferred, a, b):
+        def f(_):
+            self.assertEqual(a, b)
+        deferred.addCallback(f)
 
     def test_event(self):
         '''Test registration to events.'''
-        self.assertTrue(hasattr(dispatcher, "register"))
+        self.assertTrue(hasattr(self.disp, "register"))
 
-        a = []
-        def f():
+        def f(args):
+            self.deferredAssertEqual(self.d, args, ("esperado",))
+            self.d.callback(True)
+            return ""
 
-            a.append(True)
+        self.disp.register(events.CONNECTION_MADE, f)
+        self.disp.push(events.CONNECTION_MADE, "eserado")
+        return self.d
 
-        dispatcher.register(events.CONNECTION_MADE, f)
-        dispatcher.push(events.CONNECTION_MADE)
-        self.assertEqual(a, [True])
 
