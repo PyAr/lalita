c900fe1b081c9be40a246d0b689ad9d6b8cdb0f6
facundo@taniquetil.com.ar <>
2009-03-22 20:17:54 -0300

    More tessssssssts.

--- core/tests/test_dispatcher.py
+++ core/tests/test_dispatcher.py
@@ -18,11 +18,6 @@ class EasyDeferredTests(TwistedTestCase):
 
 
 class TestRegister(unittest.TestCase):
-
-#   * register ('event_<>', callback)
-#              ('event_command', callback, [c])
-#              ('privmsg', callback, [regex])
-#
     def setUp(self):
         super(TestRegister, self).setUp()
         self.disp = dispatcher.Dispatcher()
@@ -70,6 +65,33 @@ class TestRegister(unittest.TestCase):
 
 class TestPush(EasyDeferredTests):
 
+    def setUp(self):
+        super(TestRegister, self).setUp()
+        self.disp = dispatcher.Dispatcher()
+
+    def test_event_noarg(self):
+        '''Test pushing events with no args.'''
+        def f():
+            self.deferred.callback(True)
+            return ""
+
+        self.disp.register(events.CONNECTION_MADE, f)
+        self.disp.push(events.CONNECTION_MADE)
+        return self.deferred
+
+    def test_event_with_args(self):
+        '''Test pushing events with args.'''
+        def f(arg):
+            self.deferredAssertEqual(self.deferred, arg, "reason")
+            self.deferred.callback(True)
+            return ""
+
+        self.disp.register(events.CONNECTION_LOST, f)
+        self.disp.push(events.CONNECTION_LOST, "reason")
+        return self.deferred
+
+class TestEvents(EasyDeferredTests):
+
 #   * register ('event_<>', callback)
 #              ('event_command', callback, [c])
 #              ('privmsg', callback, [regex])
@@ -78,18 +100,71 @@ class TestPush(EasyDeferredTests):
         super(TestRegister, self).setUp()
         self.disp = dispatcher.Dispatcher()
 
-    def test_event(self):
-        '''Test pushing events.'''
-        self.assertTrue(hasattr(self.disp, "register"))
-
-        def f(args):
-            self.deferredAssertEqual(self.d, args, ("esperado",))
+    def test_connection_made(self):
+        '''Test CONNECTION_MADE.'''
+        def f():
             self.deferred.callback(True)
             return ""
 
         self.disp.register(events.CONNECTION_MADE, f)
-        self.disp.push(events.CONNECTION_MADE, "esperado")
-        self.deferred.addCallback(lambda r: self.assertEqual(r, 100))
+        self.disp.connectionMade()
         return self.deferred
 
+    def test_connection_lost(self):
+        '''Test CONNECTION_LOST.'''
+        def f(arg):
+            self.deferredAssertEqual(self.deferred, arg, "reason")
+            self.deferred.callback(True)
+            return ""
+
+        self.disp.register(events.CONNECTION_LOST, f)
+        self.disp.connectionLost("reason")
+        return self.deferred
+
+    def test_signed_on(self):
+        '''Test SIGNED_ON.'''
+        def f(arg):
+            self.deferred.callback(True)
+            return ""
+
+        self.disp.register(events.SIGNED_ON, f)
+        self.disp.signedOn()
+        return self.deferred
+
+    def test_joined(self):
+        '''Test JOINED.'''
+        def f(arg):
+            self.deferredAssertEqual(self.deferred, arg, "channel")
+            self.deferred.callback(True)
+            return ""
+
+        self.disp.register(events.JOINED, f)
+        self.disp.joined("channel")
+        return self.deferred
+
+    def test_private_message_raw(self):
+        '''Test PRIVATE_MESSAGE simple.'''
+        def f(a, b):
+            self.deferredAssertEqual(self.deferred, a, "user")
+            self.deferredAssertEqual(self.deferred, b, "msg")
+            self.deferred.callback(True)
+            return ""
+
+        self.disp.register(events.PRIVATE_MESSAGE, f)
+        self.disp.privmsg("user", "channel", "msg")
+        return self.deferred
+
+    def test_private_message_regexp(self):
+        '''Test PRIVATE_MESSAGE with regexp.'''
+        def f(a, b):
+            self.deferredAssertEqual(self.deferred, a, "user")
+            self.deferredAssertEqual(self.deferred, b, "hola mundo")
+            self.deferred.callback(True)
+            return ""
+
+        regexp = re.compile("^hola.*$")
+        self.disp.register(events.PRIVATE_MESSAGE, f, regexp)
+        self.disp.privmsg("user", "channel", "esta no pasa")
+        self.disp.privmsg("user", "channel", "hola mundo")
+        return self.deferred
 
