0d80d3fa982585d1a4b4dd6e4b62cb7541b5a5bd
Javier Mansilla <javier@algarrobo>
2009-03-23 11:17:07 -0300

    .

--- plugins/moin_search.py
+++ plugins/moin_search.py
@@ -2,7 +2,7 @@
 
 from twisted.web import client
 from BeautifulSoup import BeautifulSoup, Tag
-from core import dispatcher
+from core.dispatcher import dispatcher
 from core.events import COMMAND
 from twisted.internet import reactor
 
@@ -20,22 +20,24 @@ class MoinSearch(object):
                      {'path': '/moin?action=fullsearch&context=180&value=%s&titlesearch=T%%C3%%ADtulos',
                       'tag': 'li'}
                     }
+    _max_results = 1
 
-    def __init__(self,disp):
-        disp.register(COMMAND,self.process)
-        #dispatcher.register(COMMAND,self.process,['wiki'])
+    def __init__(self):
+        dispatcher.register(COMMAND,self.process,command_names = ['wiki'])
 
     def get_full_query(self,where,query):
         return '%s/%s' % (self._site,self._places[where]['path'] % query)
 
-    def process(self, command, user, channel, *args):
+    def search(self, command, user, channel, *args):
+        """First searchs for title, later if nothing found
+        searchs for text"""
         query = ' '.join(map(str,args))
         d = client.getPage(self.get_full_query('title',query))
         d.addCallback(self.parse_page,'title')
-        d.addCallback(self.otro,query)
+        d.addCallback(self.search_body,query)
         return d
 
-    def otro(self,titleresults,query):
+    def search_body(self,titleresults,query):
         if titleresults:
             return titleresults
         d = client.getPage(self.get_full_query('body',query))
@@ -45,10 +47,6 @@ class MoinSearch(object):
     def parse_page(self,page,where):
         print 'estoy parseando'
         soup = BeautifulSoup(page)
-        #if page.geturl() != url:
-            ## If there was only one result, moin redirected me to it
-            #title = txtize(soup.find('head').find('title'))
-            #return ['%s: %s' % (title,page.geturl())]
         div = soup.find('div','searchresults')
         results = []
         for item in div.findAll(self._places[where]['tag']):
@@ -56,90 +54,17 @@ class MoinSearch(object):
             href = self._site + anchor.get('href')
             name = txtize(anchor)
             results.append('%s: %s' % (name,href))
-        return results
+        return self.process_resutls(results)
+
+    def process_resutls(self,results):
 
 
-
-
-    def __process(self):
-        results = []
-        for place in ['title','body']: #I want first title results!
-            d = self.get_results(place)
-            d.addCallback(self.parse_page)
-
-            #results = self.get_results(place)
-            if results:
-                break
-        results_count = len(results)
-        if not results:
-            return (0,[])
-        if results_count > max_resutls:
-            return (results_count,[self.get_full_query(place)])
-        return (results_count,results)
-
-    def get_results(self,where):
-        url = self.get_full_query(where)
-        return client.getPage(url)
-
-def main():
-    disp = dispatcher.Dispatcher()
-    ms = MoinSearch(disp)
-    disp.push(COMMAND,'wiki', 'rafen', '#pyar', 'karma')
-    reactor.run()
+#def main():
+    #disp = dispatcher.Dispatcher()
+    #ms = MoinSearch(disp)
+    #disp.push(COMMAND,'wiki', 'rafen', '#pyar', 'karma')
+    #reactor.run()
 
 if __name__ == '__main__':
     main()
-
-
-#class _MoinSearch(object):
-    #_site = 'http://www.python.com.ar'
-    #_places = {'body':
-                     #{'path':'/moin?action=fullsearch&context=180&value=%s&fullsearch=Texto',
-                      #'tag': 'dt'},
-               #'title':
-                     #{'path': '/moin?action=fullsearch&context=180&value=%s&titlesearch=T%%C3%%ADtulos',
-                      #'tag': 'li'}
-                    #}
-
-    #def __init__(self,query,site=None,places=None):
-
-
-        #self.query = query
-        #if site is not None:
-            #self._site = site
-        #if places is not None:
-            #self._places = places
-
-    #def get_full_query(self,where):
-        #return '%s/%s' % (self._site,self._places[where]['path'] % self.query)
-
-    #def process(self, max_resutls=1):
-        #results = []
-        #for place in ['title','body']: #I want first title results!
-            #results = self.get_results(place)
-            #if results:
-                #break
-        #results_count = len(results)
-        #if not results:
-            #return (0,[])
-        #if results_count > max_resutls:
-            #return (results_count,[self.get_full_query(place)])
-        #return (results_count,results)
-
-    #def get_results(self,where):
-        #url = self.get_full_query(where)
-        #page = urllib.urlopen(url)
-        #soup = BeautifulSoup(page.read())
-        #if page.geturl() != url:
-            ## If there was only one result, moin redirected me to it
-            #title = txtize(soup.find('head').find('title'))
-            #return ['%s: %s' % (title,page.geturl())]
-        #div = soup.find('div','searchresults')
-        #results = []
-        #for item in div.findAll(self._places[where]['tag']):
-            #anchor = item.find('a')
-            #href = self._site + anchor.get('href')
-            #name = txtize(anchor)
-            #results.append('%s: %s' % (name,href))
-        #return results
 
