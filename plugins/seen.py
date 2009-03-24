662d57038ad68d37fd429bcc877c64522ab97d90
Marcos Dione <mdione@grulic.org.ar>
2009-03-24 17:53:46 -0300

    modified:   plugins/seen.py + implemented the seen command.

--- plugins/seen.py
+++ plugins/seen.py
@@ -13,13 +13,26 @@ class Seen (object):
     def __init__ (self, config, params):
         register= params['register']
         self.nickname= params['nickname']
-        register (events.JOIN, self.log)
-        self.config= dict (block_size=4096).update (config)
         self.seenlog= {}
+
+        register (events.JOIN, self.log)
+        register (events.COMMAND, self.seen, ['seen'])
 
     def log (self, channel, nick):
         logger.debug ("%s joined %s" % (nick, channel))
         if nick!=self.nickname:
-            self.seenlog[nick]= ("joined", time.time ())
+            self.seenlog[nick]= ('joined', time.time ())
+            logger.debug ("logged %s %s %s" % (nick, 'joined', time.time ()))
+
+    def seen (self, user, channel, command, nick):
+        # logger.debug ('seen command called w/ '+ str ())
+        # logger.debug (self.seenlog)
+        try:
+            what, when= self.seenlog[nick]
+            now= time.time ()
+            if what=='joined':
+                return (channel, "%s: le vi entrar hace un rato..." % user)
+        except KeyError:
+            return (channel, "%s: me lo deje en la otra pollera" % user)
 
 # end
