626335903150bdfabdde1aa3921d243d31decb3d
Marcos Dione <mdione@grulic.org.ar>
2009-03-26 11:37:47 -0300

    modified:   config.py + ping test on localhost.

--- config.py
+++ config.py
@@ -11,6 +11,7 @@ servers = {
             plugins= {
                 'url.Url': { },
                 'seen.Seen': { },
+                'misc.Ping': { },
             },
         ),
         'example': dict (
