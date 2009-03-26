2a779eaaf384a1ce78bef8b29810bfa977657069
Marcos Dione <mdione@grulic.org.ar>
2009-03-26 11:37:27 -0300

    added:   plugins/misc.py + misc.Ping.

---
+++ plugins/misc.py
@@ -0,0 +1,20 @@
+# -*- coding: utf8 -*-
+
+# (c) 2009 Marcos Dione <mdione@grulic.org.ar>
+
+import logging
+logger = logging.getLogger ('ircbot.plugins.misc')
+logger.setLevel (logging.DEBUG)
+
+from core import dispatcher
+from core import events
+
+class Ping (object):
+    def __init__ (self, config, params):
+        register= params['register']
+        register (events.COMMAND, self.ping, ['ping'])
+
+    def ping (self, user, channel, command):
+        return (channel, u"%s: pong" % user)
+
+# end
