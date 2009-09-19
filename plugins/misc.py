59ac73e20c1e111aa76e3ea3a34031ed6718c3c7
facundo@taniquetil.com.ar <>
2009-09-18 21:57:20 -0300

    Tests and some details.

--- plugins/misc.py
+++ plugins/misc.py
@@ -5,13 +5,13 @@
 from lalita import Plugin
 
 class Ping(Plugin):
+    '''Plugin for the "ping" functionality.'''
 
     def init(self, config):
         self.logger.debug("Init! config: %s", config)
         self.register(self.events.COMMAND, self.ping, ['ping'])
 
     def ping(self, user, channel, command):
+        u'''Sólo contesto.'''
         self.logger.debug("let's play ping pong with %s!" % user)
         self.say(channel, u"%s: pong" % user)
-
-# end
