3fb752dfa5c3ec785312282a5e46a70d1489dbaf
Gavrilov Seva <gavrilovseva@gmail.com>
2015-10-24 16:14:01 -0300

    Agregada una persistencia y corregidos los mensajes.

--- lalita/plugins/welcome.py
+++ lalita/plugins/welcome.py
@@ -4,8 +4,8 @@
 # License: GPL v3
 # For further info, see LICENSE file
 
-
-import string
+import os
+import shelve
 from lalita import Plugin
 
 
@@ -14,6 +14,20 @@ class Welcome(Plugin):
 
     def init(self, config):
         self.logger.info('Welcome plugin init! config: %s', config)
+
+        self.welcome_message = '%s: Bienvenido a %s!'
+        self.instructions_message = 'Aqui van algunas instrucciones'
+
+        # self.register_translation(self, TRANSLATION_TABLE)
+        base = config.get('basedir', None)
+        if base is not None:
+            base = os.path.join(base, config.get('channel_folder', ''))
+            if not os.path.exists(base):
+                os.makedirs(base)
+            self.known_users = shelve.open(os.path.join(base, 'users'))
+        else:
+            self.known_users = {}
+
         self.register(self.events.JOIN, self.user_joined)
         self.logged_users = []
 
@@ -22,7 +36,7 @@ class Welcome(Plugin):
         return u'$user: Bienvenido a $channel!'
 
     def new_user(self, user):
-        return user in self.logged_users
+        return user not in self.logged_users
 
     def add_user(self, user):
         self.logged_users.append(user)
@@ -30,9 +44,9 @@ class Welcome(Plugin):
     def user_joined(self, user, channel):
         if user.startswith(u'pyarense_ij'):
             self.logger.debug("%s joined %s", user, channel)
-            self.say(channel, self.default_message, user, channel)
-        else:
-            if self.new_user(user):
-                self.add_user(user)
-                self.logger.debug("%s joined %s", user, channel)
-                self.say(channel, self.default_message, user, channel)
+            self.say(channel, self.welcome_message, user, channel)
+            self.say(user, self.instructions_message)
+        elif self.new_user(user):
+            self.add_user(user)
+            self.logger.debug("%s joined %s", user, channel)
+            self.say(user, self.instructions_message)
