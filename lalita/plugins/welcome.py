f767a00f66f460c7f8778694e977521d279be852
FedeG <federicog@exo.com.ar>
2015-10-24 16:58:15 -0300

    Add support to multiple channels

--- lalita/plugins/welcome.py
+++ lalita/plugins/welcome.py
@@ -14,9 +14,8 @@ class Welcome(Plugin):
 
     def init(self, config):
         self.logger.info('Welcome plugin init! config: %s', config)
-
-        self.welcome_message = '%s: Bienvenido a %s!'
-        self.instructions_message = 'Aqui van algunas instrucciones'
+        self.welcome_message = config.get('welcome', '%s: Bienvenido a %s!')
+        self.instructions_message = config.get('instructions', 'Aqui van algunas instrucciones')
 
         # self.register_translation(self, TRANSLATION_TABLE)
         base = config.get('basedir', None)
@@ -27,21 +26,19 @@ class Welcome(Plugin):
             self.known_users = shelve.open(os.path.join(base, 'users'))
         else:
             self.known_users = {}
-
         self.register(self.events.JOIN, self.user_joined)
 
-    def new_user(self, user):
-        return user not in self.known_users
+    def new_user(self, channel, user):
+        return user not in self.known_users or channel not in self.known_users[user]
 
     def add_user(self, channel, user):
-        self.known_users[user] = channel
+        self.known_users[user][channel] = user
 
     def user_joined(self, user, channel):
+        self.logger.debug("%s joined %s", user, channel)
         if user.startswith(u'pyarense'):
-            self.logger.debug("%s joined %s", user, channel)
             self.say(channel, self.welcome_message, user, channel)
             self.say(user, self.instructions_message)
-        elif self.new_user(user):
+        elif self.new_user(channel, user):
             self.add_user(channel, user)
-            self.logger.debug("%s joined %s", user, channel)
             self.say(channel, self.welcome_message, user, channel)
